c6a20914810078c6fe6cb659277b85ec6b8136da
Ashures <ashuresyt@gmail.com>
2023-04-11 19:33:54 +1200

    Fixed incorrect modid value for 'MOD_ID' constant

--- src/main/java/net/ashures/universalmod/UniversalMod.java
+++ src/main/java/net/ashures/universalmod/UniversalMod.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class UniversalMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+	public static final String MOD_ID = "universalmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
