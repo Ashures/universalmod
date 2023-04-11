3eae14a76949de52124cd54386c1b573dc46429a
Ashures <ashuresyt@gmail.com>
2023-04-11 19:29:09 +1200

    Initial commit

---
+++ src/main/java/net/ashures/universalmod/UniversalMod.java
@@ -0,0 +1,15 @@
+package net.ashures.universalmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UniversalMod implements ModInitializer {
+	public static final String MOD_ID = "modid";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
