37d3228bc0c7d4b68ca72fc9a060cb0186af8e55
Ashures <ashuresyt@gmail.com>
2023-04-16 11:39:05 +1200

    Fixed client vs. server method call bugs and changed parent class to 'Block' type

--- src/main/java/net/ashures/universalmod/block/custom/LaunchPadBlock.java
+++ src/main/java/net/ashures/universalmod/block/custom/LaunchPadBlock.java
@@ -1,29 +1,45 @@
 package net.ashures.universalmod.block.custom;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CarpetBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
-public class LaunchPadBlock extends CarpetBlock {
+public class LaunchPadBlock extends Block {
     public LaunchPadBlock(Settings settings) {
         super(settings);
     }
 
     @Override
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
-        if (!world.isClient()) {
-            if (entity instanceof LivingEntity) {
-                LivingEntity livingEntity = (LivingEntity) entity;
-                livingEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 300, 5, true, false));
-                livingEntity.setJumping(true);
-            }
-        }
+        bounce(entity);
 
         super.onSteppedOn(world, pos, state, entity);
+    }
+
+    @Override
+    public void onLandedUpon(World world, BlockState state, BlockPos pos, Entity entity, float fallDistance) {
+        if (entity.bypassesLandingEffects()) {
+            super.onLandedUpon(world, state, pos, entity, fallDistance);
+        } else {
+            entity.handleFallDamage(fallDistance, 0.0f, DamageSource.FALL);
+        }
+    }
+
+    public void bounce(Entity entity) {
+        double reboundMultiplier = entity instanceof LivingEntity? 1.0 : 0.5;
+        Vec3d currentVel = entity.getVelocity();
+
+        entity.setVelocity(new Vec3d(currentVel.x, 2.0 * reboundMultiplier, currentVel.z));
     }
 }
