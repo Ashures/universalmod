dcf08b7f2712f78833f58f70cc073f18b78c460e
Ashures <ashuresyt@gmail.com>
2023-04-17 21:58:31 +1200

    Fixed broken logic in 'LightningStaffItem'

--- src/main/java/net/ashures/universalmod/item/custom/LightningStaffItem.java
+++ src/main/java/net/ashures/universalmod/item/custom/LightningStaffItem.java
@@ -1,14 +1,18 @@
 package net.ashures.universalmod.item.custom;
 
-import net.minecraft.entity.Entity;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.FireBlock;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LightningEntity;
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.DragonFireballEntity;
+import net.minecraft.entity.projectile.FireballEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUsageContext;
-import net.minecraft.predicate.entity.LightningBoltPredicate;
-import net.minecraft.util.ActionResult;
+import net.minecraft.text.Text;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
@@ -20,20 +24,23 @@ public class LightningStaffItem extends Item {
     }
 
     @Override
-    public ActionResult useOnBlock(ItemUsageContext context) {
-        World world = context.getWorld();
-        BlockPos blockPos = context.getBlockPos();
-        Vec3d spawnPos = new Vec3d(blockPos.getX(), blockPos.getY(), blockPos.getZ());
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        HitResult blockCheck = user.raycast(40, 0, true);
+        BlockState hitBlock = world.getBlockState(new BlockPos(blockCheck.getPos()));
 
-        castLightning(world, spawnPos);
+        if (!hitBlock.getBlock().equals(Blocks.AIR)) {
+            user.sendMessage(hitBlock.getBlock().getName(), false);
+            castSpell(world, blockCheck.getPos(), user);
+            return TypedActionResult.success(user.getMainHandStack(), true);
+        }
 
-        return super.useOnBlock(context);
+        return super.use(world, user, hand);
     }
 
-    public void castLightning(World world, Vec3d spawnPos) {
-        LightningEntity lightning = new LightningEntity(EntityType.LIGHTNING_BOLT, world);
-        lightning.setPosition(spawnPos);
+    public void castSpell(World world, Vec3d spawnPos, PlayerEntity player) {
+        LightningEntity entity = new LightningEntity(EntityType.LIGHTNING_BOLT, world);
+        entity.setPosition(spawnPos);
 
-        world.spawnEntity(lightning);
+        world.spawnEntity(entity);
     }
 }
