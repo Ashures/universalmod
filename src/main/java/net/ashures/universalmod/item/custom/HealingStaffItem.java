93ce4f75decada96b20ce7466e8d4cdb78ed2147
Ashures <ashuresyt@gmail.com>
2023-04-19 00:15:59 +1200

    Increased spell potency of 'HealingStaffItem'

--- src/main/java/net/ashures/universalmod/item/custom/HealingStaffItem.java
+++ src/main/java/net/ashures/universalmod/item/custom/HealingStaffItem.java
@@ -1,10 +1,16 @@
 package net.ashures.universalmod.item.custom;
 
+import net.ashures.universalmod.item.ModItems;
 import net.ashures.universalmod.sound.ModSounds;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.FireballEntity;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
@@ -27,7 +33,13 @@ public class HealingStaffItem extends Item {
     public void castSpell(World world, PlayerEntity player) {
         float healAmount = player.getMaxHealth() - player.getHealth();
         BlockPos playerLocation = new BlockPos(player.getPos());
+
         player.heal(healAmount);
+        player.addStatusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 600, 4, false, false));
+        player.addStatusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 200, 4, false, false));
+        player.addStatusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 600, 0, false, false));
+        player.eatFood(world, new ItemStack(ModItems.DIVINE_APPLE));
+
         world.playSound(player, playerLocation, ModSounds.HEALING_STAFF_SUCCESS, SoundCategory.PLAYERS, 1f, 1f);
     }
 }
