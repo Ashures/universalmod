6cb54cefcb9fc0780d69f55508d510f3640b20b5
Ashures <ashuresyt@gmail.com>
2023-04-20 21:03:34 +1200

    Added new 'ChaosGreatSwordItem' SwordItem class with accompanying new ToolMaterial

---
+++ src/main/java/net/ashures/universalmod/item/ModToolMaterials.java
@@ -0,0 +1,60 @@
+package net.ashures.universalmod.item;
+
+import net.fabricmc.yarn.constants.MiningLevels;
+import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.tag.ItemTags;
+import net.minecraft.util.Lazy;
+
+import java.util.function.Supplier;
+
+public enum ModToolMaterials implements ToolMaterial {
+    CHAOS(5, 4062, 11.0f, 6.0f, 35, () -> Ingredient.ofItems(Items.NETHER_STAR));
+
+    private final int miningLevel;
+    private final int itemDurability;
+    private final float miningSpeed;
+    private final float attackDamage;
+    private final int enchantability;
+    private final Lazy<Ingredient> repairIngredient;
+
+    private ModToolMaterials(int miningLevel, int itemDurability, float miningSpeed, float attackDamage, int enchantability, Supplier<Ingredient> repairIngredient) {
+        this.miningLevel = miningLevel;
+        this.itemDurability = itemDurability;
+        this.miningSpeed = miningSpeed;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairIngredient = new Lazy<Ingredient>(repairIngredient);
+    }
+
+    @Override
+    public int getDurability() {
+        return this.itemDurability;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return this.miningSpeed;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return this.attackDamage;
+    }
+
+    @Override
+    public int getMiningLevel() {
+        return this.miningLevel;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+}
---
+++ src/main/java/net/ashures/universalmod/item/custom/ChaosGreatSwordItem.java
@@ -0,0 +1,60 @@
+package net.ashures.universalmod.item.custom;
+
+import net.minecraft.entity.*;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
+
+public class ChaosGreatSwordItem extends SwordItem {
+    private final int LIGHTNING_SPAWN_COUNT = 5;
+
+    public ChaosGreatSwordItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
+        super(toolMaterial, attackDamage, attackSpeed, settings);
+    }
+
+    @Override
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        World world = target.getWorld();
+        LightningEntity lightningEntity = new LightningEntity(EntityType.LIGHTNING_BOLT, world);
+        lightningEntity.setPosition(target.getPos());
+
+        for (int i = 0; i < LIGHTNING_SPAWN_COUNT; i++) {
+            world.spawnEntity(lightningEntity);
+        }
+
+        if (!target.isAlive()) {
+            int currentAmplifier = 0;
+            if (attacker.hasStatusEffect(StatusEffects.STRENGTH)) {
+                currentAmplifier = attacker.getStatusEffect(StatusEffects.STRENGTH).getAmplifier() + 1;
+            }
+
+            attacker.addStatusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 200, currentAmplifier));
+        }
+        stack.damage(1, attacker, e -> e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
+        return super.postHit(stack, target, attacker);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        HitResult raycast = user.raycast(30, 1, true);
+        Vec3d blockCheck = raycast.getPos();
+
+        if (!(raycast.getType() == HitResult.Type.MISS)) {
+            TntEntity tntEntity = new TntEntity(world, blockCheck.x, blockCheck.y, blockCheck.z, user);
+            tntEntity.setFuse(0);
+            world.createExplosion(tntEntity, tntEntity.getX(), tntEntity.getBodyY(0.0625), tntEntity.getZ(), 20.0f, Explosion.DestructionType.DESTROY);
+            return TypedActionResult.success(user.getMainHandStack(), true);
+        }
+
+        return super.use(world, user, hand);
+    }
+}
