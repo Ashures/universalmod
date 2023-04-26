e4048b73b889c1c3b47b1f2aad3ac23d974809e3
Ashures <ashuresyt@gmail.com>
2023-04-26 22:37:11 +1200

    Added 'WORLD_EATER' item to increase/decrease key inputs and changed 'explosion' to 'spell' in variable names

--- src/main/java/net/ashures/universalmod/event/KeyInputHandler.java
+++ src/main/java/net/ashures/universalmod/event/KeyInputHandler.java
@@ -2,25 +2,25 @@ package net.ashures.universalmod.event;
 
 import net.ashures.universalmod.item.ModItems;
 import net.ashures.universalmod.item.custom.FireStaffItem;
+import net.ashures.universalmod.item.custom.WorldEaterItem;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.item.ItemStack;
-import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyInputHandler {
     public static final String KEY_CATEGORY_UNIVERSAL = "key.category.universalmod.universal";
-    public static final String KEY_INCREASE_EXPLOSION = "key.universalmod.increase_explosion";
-    public static final String KEY_DECREASE_EXPLOSION = "key.universalmod.decrease_explosion";
+    public static final String KEY_INCREASE_POWER = "key.universalmod.increase_power";
+    public static final String KEY_DECREASE_POWER = "key.universalmod.decrease_power";
 
-    public static KeyBinding increaseExplosionKey;
-    public static KeyBinding decreaseExplosionKey;
+    public static KeyBinding increasePowerKey;
+    public static KeyBinding decreasePowerKey;
 
     public static void registerKeyInputs() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (increaseExplosionKey.wasPressed()) {
+            if (increasePowerKey.wasPressed()) {
                 if (!client.player.getMainHandStack().isEmpty()) {
                     ItemStack itemStack = client.player.getMainHandStack();
                     if (itemStack.getItem() == ModItems.FIRE_STAFF) {
@@ -29,9 +29,15 @@ public class KeyInputHandler {
 
                         item.setExplosionPower(currentPower + 1, client.player);
                     }
+                    if (itemStack.getItem() == ModItems.WORLD_EATER) {
+                        WorldEaterItem item = (WorldEaterItem) itemStack.getItem();
+                        int currentPower = item.getSpellPower();
+
+                        item.setSpellPower(currentPower + 1, client.player);
+                    }
                 }
             }
-            if (decreaseExplosionKey.wasPressed()) {
+            if (decreasePowerKey.wasPressed()) {
                 if (!client.player.getMainHandStack().isEmpty()) {
                     ItemStack itemStack = client.player.getMainHandStack();
                     if (itemStack.getItem() == ModItems.FIRE_STAFF) {
@@ -40,20 +46,26 @@ public class KeyInputHandler {
 
                         item.setExplosionPower(currentPower - 1, client.player);
                     }
+                    if (itemStack.getItem() == ModItems.WORLD_EATER) {
+                        WorldEaterItem item = (WorldEaterItem) itemStack.getItem();
+                        int currentPower = item.getSpellPower();
+
+                        item.setSpellPower(currentPower - 1, client.player);
+                    }
                 }
             }
         });
     }
 
     public static void register() {
-        increaseExplosionKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                KEY_INCREASE_EXPLOSION,
+        increasePowerKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                KEY_INCREASE_POWER,
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_PERIOD,
                 KEY_CATEGORY_UNIVERSAL
         ));
-        decreaseExplosionKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                KEY_DECREASE_EXPLOSION,
+        decreasePowerKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                KEY_DECREASE_POWER,
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_COMMA,
                 KEY_CATEGORY_UNIVERSAL
